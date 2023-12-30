0967c17c0a109cd168ae8147f93cb9e5ee309ed4
Ranuga <79456372+Programmer-RD-AI@users.noreply.github.com>
2023-12-30 23:15:22 +0530

    Create string_compare.py

---
+++ string_compare.py
@@ -0,0 +1,33 @@
+def lex(value: str) -> int:
+    alphabet = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
+    tot = 0
+    for letter in value:
+        tot += alphabet.index(letter)
+    return tot
